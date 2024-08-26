da3dc84bc6e6b534353fa4fdaa15266da5ed2811
Maria Cosenza <127864622+BellaPippin@users.noreply.github.com>
2024-08-26 16:37:55 -0500

    First steps w JavaScript

---
+++ learn02-start/scripts/script.js
@@ -0,0 +1,13 @@
+function myFunction2(){
+        
+    document.getElementById('sample').innerHTML = 'Changed from a function in an external file';
+}
+
+// Single line comment
+
+/*
+
+Block comment
+
+*/
+
