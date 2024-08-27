6ce326b08e8bb5f310ca26c9114614103e057be3
Maria Cosenza <127864622+BellaPippin@users.noreply.github.com>
2024-08-26 19:41:29 -0500

    Mod1Assignment01

---
+++ assignment01/scripts/script.js
@@ -0,0 +1,5 @@
+function myFunction() {
+
+    document.getElementById('paragraph').innerHTML = 'Lorem Ipsum is a type of placeholder text commonly used in the design and publishing industries. Its origins trace back to a scrambled passage of Latin text from a work by Cicero, a Roman orator, dating back to 45 BC. The text is used to create a natural-looking distribution of letters and words, which helps designers and developers focus on the layout and visual elements of a project without being distracted by meaningful content. By using Lorem Ipsum, one can ensure that design elements such as typography, spacing, and alignment are evaluated without the influence of actual text. This makes it a versatile tool in mockups and drafts before the final content is added.' ;
+
+}
